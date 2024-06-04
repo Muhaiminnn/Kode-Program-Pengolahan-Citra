52cd4f6d53cacc24b022f1d2780c0f5ad7ac1ef5
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-06-04 14:50:47 +0800

    Update citraGrayscale.py

--- 02. Citra Grayscale & Citra RGB/citraGrayscale.py
+++ 02. Citra Grayscale & Citra RGB/citraGrayscale.py
@@ -1,1 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
 
+# Baca citra
+img = cv2.imread('example_citra.jpg')
+
+# Ubah citra keabuan
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Tampilkan citra asli dan citra keabuan
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title('Citra Asli')
+
+plt.subplot(1, 2, 2)
+plt.imshow(grayscale_img, cmap='gray')
+plt.title('Citra Keabuan')
+plt.show()
