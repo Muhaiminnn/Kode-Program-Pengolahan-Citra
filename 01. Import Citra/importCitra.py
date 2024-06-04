24501875a67b89941294b041fcb7d71a4b31a2ed
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-06-04 14:48:59 +0800

    Update importCitra.py

--- 01. Import Citra/importCitra.py
+++ 01. Import Citra/importCitra.py
@@ -1,1 +1,8 @@
-import cv2
+import cv2 # memanggil library OpenCV
+from google.colab.patches import cv2_imshow
+
+# sesuaikan dengan nama file
+img = cv2.imread("example_citra.jpg")
+
+# menampilkan gambar
+cv2_imshow(img)
